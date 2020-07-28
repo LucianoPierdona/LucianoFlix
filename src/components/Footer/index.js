eac1f0486d45c7d966875da7ee54477f1179cf82
Luciano Pierdona <lucianompj1@gmail.com>
2020-07-27 22:45:16 -0300

    Day 1 - Update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Proudly created in the
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          React Immersion from Alura
         </a>
       </p>
     </FooterBase>
